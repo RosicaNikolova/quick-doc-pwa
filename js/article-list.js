dcab9c16c448c27633eab93108bd548db9fdcb93
Vasil <vasil.todorov@student.fontys.nl>
2023-03-13 15:02:37 +0100

    finish article generation

--- js/article-list.js
+++ js/article-list.js
@@ -2,7 +2,7 @@ for (let i = 0; i < 5; i++) {
     getArticles(i);
     document.querySelector(".inqueue-article-container").innerHTML += `
     <div class="inqueue-article">
-        <img class="inqueue-article-image" src="./images/background1.png" alt="article-image">
+        <img class="inqueue-article-image" id="img${i}"src="./images/background1.png" alt="article-image">
         <div class="inqueue-tag">
             <p id="tag${i}">Did you know?</p>
         </div>
@@ -16,8 +16,7 @@ function getArticles(number){
     fetch("./article.json")
       .then((res) => res.json())
       .then((data) => {
-        console.log(data.articles[number].id);
-        
+        document.getElementById(`img${number}`).src = data.articles[number].image;
         document.getElementById(`tag${number}`).innerHTML = data.articles[number].tag;
         document.getElementById(`title${number}`).innerHTML = data.articles[number].title;
 
