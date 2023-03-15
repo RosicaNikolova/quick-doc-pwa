1ea0e19c4e57847eeda2f59372c185e85041e741
RosicaNikolova <rosicanikolova1402@gmail.com>
2023-03-15 14:36:14 +0100

    resourced cached

--- service-worker.js
+++ service-worker.js
@@ -1,8 +1,46 @@
 
+const staticCacheName = 'site-static';
+const assets = [
+    '/',
+    '/index.html',
+    '/article.html',
+    '/finish.html',
+    '/home.html',
+    '/inqueue.html',
+    '/js/index.js',
+    '/js/article-list.js',
+    '/js/finish.js',
+    '/js/timer.js',
+    '/js/install-sw.js',
+    '/css/article.css',
+    '/css/finish.css',
+    '/css/home.css',
+    '/css/index.css',
+    '/css/inqueue.css',
+    '/images/article1.png',
+    '/images/article2.png',
+    '/images/article3.png',
+    '/images/article4.png',
+    '/images/article5.png',
+    '/images/back-icon.svg',
+    '/images/background1.png',
+    '/images/doctor.jpg',
+    '/images/face-frown-regular.svg',
+    '/images/user-doctor-solid.svg',
+    '/images/pexels-cottonbro-studio-5079274.jpg',
+    'article.json'
+];
 //self - refers to the service worker itself
 
   //install service worker
-  self.addEventListener("install",(installing)=>{
+  self.addEventListener("install", installing => {
+    installing.waitUntil(
+          //asynchronous task
+      caches.open(staticCacheName).then(cache =>{
+        console.log('caching assets');
+        cache.addAll(assets)
+      })
+    );
     console.log("Service Worker: I am being installed, hello world!");
   });
   
@@ -13,7 +51,7 @@
   
   //fetch html pages. css files and so on
   self.addEventListener("fetch",(fetching)=>{   
-    console.log("Service Worker: User threw a ball, I need to fetch it!", fetching);
+    //console.log("Service Worker: User threw a ball, I need to fetch it!", fetching);
   });
   
   self.addEventListener("push",(pushing)=>{
