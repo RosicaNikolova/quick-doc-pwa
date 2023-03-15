71403db19cb38601d31829c53acb2ff53408f4d8
RosicaNikolova <rosicanikolova1402@gmail.com>
2023-03-15 17:27:44 +0100

    dynamic caching implemented

--- service-worker.js
+++ service-worker.js
@@ -1,34 +1,36 @@
 
 const staticCacheName = 'site-static-v1';
+const dynamicCache = 'site-dymanic-v3';
 const assets = [
     '/',
     '/index.html',
-    '/article.html',
-    '/finish.html',
-    '/home.html',
-    '/inqueue.html',
+    '/fallback.html',
+    // '/article.html',
+    // '/finish.html',
+    // '/home.html',
+    // '/inqueue.html',
     '/js/index.js',
-    '/js/article-list.js',
-    '/js/finish.js',
-    '/js/timer.js',
-    '/js/install-sw.js',
-    '/css/article.css',
-    '/css/finish.css',
-    '/css/home.css',
-    '/css/index.css',
-    '/css/inqueue.css',
-    '/images/article1.png',
-    '/images/article2.png',
-    '/images/article3.png',
-    '/images/article4.png',
-    '/images/article5.png',
-    '/images/back-icon.svg',
-    '/images/background1.png',
-    '/images/doctor.jpg',
-    '/images/face-frown-regular.svg',
-    '/images/user-doctor-solid.svg',
-    '/images/pexels-cottonbro-studio-5079274.jpg',
-    'article.json'
+    // '/js/article-list.js',
+    // '/js/finish.js',
+    // '/js/timer.js',
+     '/js/install-sw.js',
+    // '/css/article.css',
+    // '/css/finish.css',
+    // '/css/home.css',
+     '/css/index.css',
+    // '/css/inqueue.css',
+    // '/images/article1.png',
+    // '/images/article2.png',
+    // '/images/article3.png',
+    // '/images/article4.png',
+    // '/images/article5.png',
+    // '/images/back-icon.svg',
+    // '/images/background1.png',
+    // '/images/doctor.jpg',
+    // '/images/face-frown-regular.svg',
+    // '/images/user-doctor-solid.svg',
+    // '/images/pexels-cottonbro-studio-5079274.jpg',
+    // 'article.json'
 ];
 //self - refers to the service worker itself
 
@@ -49,7 +51,7 @@ const assets = [
     activating.waitUntil(
       caches.keys().then(keys => {
         return Promise.all(keys
-          .filter(key => key !== staticCacheName)
+          .filter(key => key !== staticCacheName && key !== dynamicCache)
           .map(key => caches.delete(key))
           )
       })
@@ -61,8 +63,13 @@ const assets = [
   self.addEventListener("fetch", fetching => {
     fetching.respondWith(
       caches.match(fetching.request).then(cacheRes => {
-        return cacheRes || fetch(fetching.request);
-      })
+        return cacheRes || fetch(fetching.request).then(fetchRes => {
+          return caches.open(dynamicCache).then(cache => {
+            cache.put(fetching.request.url, fetchRes.clone())
+            return fetchRes;
+          })
+        });
+      }).catch(() => caches.match('/fallback.html'))
     );
     //console.log("Service Worker: User threw a ball, I need to fetch it!", fetching);
   });
